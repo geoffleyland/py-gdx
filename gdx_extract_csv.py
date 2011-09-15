4ce123548e95992a4ad7ea11ff481ba0bed8e9c2
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-09-15 14:57:43 +1200

    gdx_extract_csv can extract all the symbols that have the specified dimensions

--- gdx_extract_csv.py
+++ gdx_extract_csv.py
@@ -76,66 +76,19 @@ def write_parameter(s, header, row_map, values, stype="notset", address=None):
         write_value(s, header, row_map, values, address)
 
 
-def write_report(files, symbol_names, output=None, gams_dir=None):
+def write_report(filesymbols, symbols1, symbol_names, output=None):
     if not output: output = sys.stdout
 
+    # Generate all the lines for the report
     row_map = {} 
     header_map = {}
     values = {}
 
-    filesymbols = {}
-    symbols1 = None
-
-    # Read all the symbols from all the files
-    for f in files:
-        filesymbols[f] = gdxdict.read(f, gams_dir)
-        if not symbols1:
-            symbols1 = filesymbols[f]
-        else:
-            gdxdict.merge_UELs(symbols1, filesymbols[f])
-
-    # Check the domains of all the symbols
-    potential_domains = []
-    for sn in symbol_names:
-        for f in files:
-            info = gdxdict.get_symbol_info(filesymbols[f], sn)
-            for d in range(info["dims"]):
-                if d >= len(potential_domains):
-                    pd = {}
-                    potential_domains.append(pd)
-                    for a in info["domain"][d]["ancestors"]:
-                        if a != "*":
-                            pd[a] = gdxdict.get_symbol_info(filesymbols[f], a)["records"]
-                else:
-                    pd = potential_domains[d]
-                    sd = {}
-                    for a in info["domain"][d]["ancestors"]:
-                        sd[a] = gdxdict.get_symbol_info(filesymbols[f], a)["records"]
-                    remove = []
-                    for a in pd:
-                        if a in sd:
-                            pd[a] = max(sd[a], pd[a])
-                        else:
-                            remove.append(a)
-                    for r in remove: del pd[r]
-
-    domains = []
-    for pd in potential_domains:
-        if len(pd) == 0:
-            raise extract_error("Domains of the symbols do not match: you need to run the script more than once to generate more than one CSV file")
-        smallest_set = None
-        least = 1e9
-        for s in pd:
-            if pd[s] < least:
-                least = pd[s]
-                smallest_set = s
-        domains.append(smallest_set)
-
-    for f in files:
+    for f in filesymbols:
         symbols = filesymbols[f]
         for sn in symbol_names:
             if sn in symbols:
-                if len(files) > 1:
+                if len(filesymbols) > 1:
                     n, dummy = os.path.splitext(os.path.basename(f))
                     header = n + ": " + sn
                 else:
@@ -184,6 +137,105 @@ def write_report(files, symbol_names, output=None, gams_dir=None):
         output.write("\n")
 
 
+def read_files(files, gams_dir=None):
+    filesymbols = {}
+    symbols1 = None
+
+    # Read all the symbols from all the files
+    for f in files:
+        filesymbols[f] = gdxdict.read(f, gams_dir)
+        if not symbols1:
+            symbols1 = filesymbols[f]
+        else:
+            gdxdict.merge_UELs(symbols1, filesymbols[f])
+
+    return filesymbols, symbols1
+
+
+def write_symbol_report(files, symbol_names, output=None, gams_dir=None):
+#    if not output: output = sys.stdout
+
+    filesymbols, symbols1 = read_files(files, gams_dir)
+
+    # Check the domains of all the symbols
+    potential_domains = []
+    for sn in symbol_names:
+        for f in files:
+            info = gdxdict.get_symbol_info(filesymbols[f], sn)
+            for d in range(info["dims"]):
+                if d >= len(potential_domains):
+                    pd = {}
+                    potential_domains.append(pd)
+                    for a in info["domain"][d]["ancestors"]:
+                        if a != "*":
+                            pd[a] = gdxdict.get_symbol_info(filesymbols[f], a)["records"]
+                else:
+                    pd = potential_domains[d]
+                    sd = {}
+                    for a in info["domain"][d]["ancestors"]:
+                        sd[a] = gdxdict.get_symbol_info(filesymbols[f], a)["records"]
+                    remove = []
+                    for a in pd:
+                        if a in sd:
+                            pd[a] = max(sd[a], pd[a])
+                        else:
+                            remove.append(a)
+                    for r in remove: del pd[r]
+
+    domains = []
+    for pd in potential_domains:
+        if len(pd) == 0:
+            raise extract_error("Domains of the symbols do not match: you need to run the script more than once to generate more than one CSV file")
+        smallest_set = None
+        least = 1e9
+        for s in pd:
+            if pd[s] < least:
+                least = pd[s]
+                smallest_set = s
+        domains.append(smallest_set)
+
+    write_report(filesymbols, symbols1, symbol_names, output)
+
+
+def write_dimension_report(files, dimensions, output=None, gams_dir=None):
+    filesymbols, symbols1 = read_files(files, gams_dir)
+
+    possible_symbols = {}
+    for f in filesymbols:
+        for k in filesymbols[f]:
+            possible_symbols[k] = True
+        break
+
+    # Find all the symbols that have the specified dimensions
+    for f in filesymbols:
+        symbols = filesymbols[f]
+        remove = []
+        for k in possible_symbols:
+            if not k in symbols:
+                remove.append(k)
+                continue
+            info = gdxdict.get_symbol_info(symbols, k)
+            if info["dims"] != len(dimensions):
+                remove.append(k)
+                continue
+            for i in range(len(dimensions)):
+                ok = False
+                for a in info["domain"][i]["ancestors"]:
+                    if a == dimensions[i]:
+                        ok = True
+                        break
+                if not ok:
+                    remove.append(k)
+                    break
+
+        for r in remove: del possible_symbols[r]
+
+    symbol_names = []
+    for s in possible_symbols: symbol_names.append(s)    
+
+    write_report(filesymbols, symbols1, symbol_names, output)
+
+
 #- main ------------------------------------------------------------------------
 
 def main(argv=None):
@@ -192,8 +244,9 @@ def main(argv=None):
 
     parser = optparse.OptionParser(usage =
 """python %prog [options]
-Produce a csv file containing the values of selected sets or parameters from one
-or more gdx files.
+Produce a csv file containing the values of selected symbols from one
+or more gdx files.  Symbols can be selected by name or by selecting all the
+symbols with a set of dimensions.
 
 Examples:
 python %prog -f days.gdx -s daynumber -s isweekend
@@ -213,14 +266,15 @@ Denmark, 70032, 124781
     parser.add_option("-f", "--file", help="Add a gdx file to read from", action="append", dest="files")
     parser.add_option("-d", "--directory", help="Add a directory to read several gdx files from", action="append", dest="directories")
     parser.add_option("-s", "--symbol", help="Add a symbol to the report", action="append", dest="symbols")
+    parser.add_option("-D", "--dimension", help="Add a dimension to the report", action="append", dest="dimensions")
     parser.add_option("-o", "--output", help="Where to write the output csv file (default is to the console)", default=None)
     parser.add_option("-g", "--gams-dir", help="Specify the GAMS installation directory if it isn't found automatically", default=None)
 
     try:
         options, args = parser.parse_args(argv)
 
-        if not options.symbols:
-            parser.error("No symbols specified (try python %s --help)" % args[0])
+        if not options.dimensions and not options.symbols:
+            parser.error("No symbols or dimensions specified (try python %s --help)" % args[0])
 
         files = options.files
         if not files: files = []
@@ -233,12 +287,18 @@ Denmark, 70032, 124781
         if len(files) == 0:
             parser.error("No files or directories specified (try python %s --help)" % args[0])
 
+        if options.dimensions and options.symbols:
+            print >>sys.stderr, "Both dimension and symbol names specified: using dimensions and ignoring symbols"
+
         if options.output:
             outfile = open(options.output, "w")
         else:
             outfile = sys.stdout
 
-        write_report(files, options.symbols, outfile, options.gams_dir)
+        if options.dimensions:
+            write_dimension_report(files, options.dimensions, outfile, options.gams_dir)
+        else:
+            write_symbol_report(files, options.symbols, outfile, options.gams_dir)
 
     except (optparse.OptionError, TypeError), err:
         print >>sys.stderr, err
